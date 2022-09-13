e8193b63b5e61ab96c3ba706f4c9bcca61d4cea7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-13 17:24:11 +0300

    bug fixed: 1- spinner bug. 2- phone load bug. 3- phone details url bug 4-search by enter bug 4- phone details data loading bug

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
+const searchField = document.getElementById('search-field');
 const loadPhones = async(searchText, dataLimit) =>{
+    //console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
+    //console.log(data);
     displayPhones(data.data, dataLimit);
 }
 
 const displayPhones = (phones, dataLimit) =>{
+    //console.log(phones);
     const phonesContainer = document.getElementById('phones-container');
-    // phonesContainer.textContent = '';
+    phonesContainer.textContent = '';
     // display 10 phones only 
     const showAll = document.getElementById('show-all');
     if(dataLimit && phones.length > 10) {
@@ -18,7 +22,6 @@ const displayPhones = (phones, dataLimit) =>{
         showAll.classList.add('d-hidden');
     }
     
-
     // display no phones found
     const noPhone = document.getElementById('no-found-message');
     if(phones.length === 0){
@@ -29,11 +32,12 @@ const displayPhones = (phones, dataLimit) =>{
     }
     // display all phones
     phones.forEach(phone =>{
+        //console.log(phone);
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
-        phonesContainer.innerHTML = `
+        phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -50,27 +54,30 @@ const displayPhones = (phones, dataLimit) =>{
 
 const processSearch = (dataLimit) =>{
     toggleSpinner(true);
-    const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText, dataLimit);
+    
 }
 
 // handle search button click
-document.getElementById('#btn-search').addEventListener('click', function(){
+document.getElementById('btn-search').addEventListener('click', function(){
+    console.log('sdkafjsdklfh');
+    searchField.value = '';
     // start loader
     processSearch(10);
 })
 
 // search input field enter key handler
 document.getElementById('search-field').addEventListener('keypress', function (e) {
-    if (e.key === 'enter') {
+    console.log(e.key);
+    if (e.key === 'Enter') {
         processSearch(10);
     }
 });
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(!isLoading){
+    if(isLoading){
         loaderSection.classList.remove('d-none')
     }
     else{
@@ -84,10 +91,12 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch();
 })
 
-const loadPhoneDetails = async id =>{
-    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
+const loadPhoneDetails = async (id) =>{
+    const url =` https://openapi.programming-hero.com/api/phone/${id}`;
+    console.log(url);
     const res = await fetch(url);
     const data = await res.json();
+    console.log(data);
     displayPhoneDetails(data.data);
 }
 
@@ -98,8 +107,8 @@ const displayPhoneDetails = phone =>{
     const phoneDetails = document.getElementById('phone-details');
     console.log(phone.mainFeatures.sensors[0]);
     phoneDetails.innerHTML = `
-        <p>Release Date: ${phone.releaseDate}</p>
-        <p>Storage: ${phone.mainFeatures}</p>
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No data found'}</p>
+        <p>Storage: ${phone.mainFeatures.storage }</p>
         <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
         <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     `
